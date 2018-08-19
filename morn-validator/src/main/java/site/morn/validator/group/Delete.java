6aa65bd1ca1fc0eec662c530f12d3378bcbe213c
timely-rain <lolic_sd@hotmail.com>
2018-08-19 20:13:56 +0800

    Initial

---
+++ morn-validator/src/main/java/site/morn/validator/group/Delete.java
@@ -0,0 +1,11 @@
+package site.morn.validator.group;
+
+/**
+ * 删除
+ *
+ * @author timely-rain
+ * @version 1.0.0, 2018/8/16
+ * @since 1.0
+ */
+public interface Delete {
+}
---
+++ morn-validator/src/main/java/site/morn/validator/group/Update.java
@@ -0,0 +1,11 @@
+package site.morn.validator.group;
+
+/**
+ * 全量更新
+ *
+ * @author timely-rain
+ * @version 1.0.0, 2018/8/16
+ * @since 1.0
+ */
+public interface Update {
+}
