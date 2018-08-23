509af286c553e0aac46b2237f58b2212ffd48578
timely-rain <lolic_sd@hotmail.com>
2018-08-23 23:04:23 +0800

    标签注解

---
+++ morn-core/src/main/java/site/timely/tag/annotation/Tag.java
@@ -0,0 +1,24 @@
+package site.timely.tag.annotation;
+
+/**
+ * 标记注解
+ *
+ * @author timely-rain
+ * @version 1.0.0, 2018/8/19
+ * @since 1.0
+ */
+public @interface Tag {
+    /**
+     * 获取名称标记
+     *
+     * @return 名称标记
+     */
+    String[] names() default {};
+
+    /**
+     * 获取类型标记
+     *
+     * @return 类型标记
+     */
+    Class<?>[] targets() default {};
+}
