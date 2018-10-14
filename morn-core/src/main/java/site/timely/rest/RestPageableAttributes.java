2a5228d3a142b3280f40e0bcedd39c4c5c0fe503
timely-rain <lolic_sd@hotmail.com>
2018-10-14 18:34:53 +0800

    Rest分页

---
+++ morn-core/src/main/java/site/timely/rest/RestPageableAttributes.java
@@ -0,0 +1,51 @@
+package site.timely.rest;
+
+
+import site.timely.core.CriteriaAttributes;
+
+/**
+ * REST分页参数
+ *
+ * @author timely-rain
+ * @version 1.0.0, 2018/7/10
+ * @see CriteriaAttributes 标准属性类
+ * @since 1.0
+ */
+public interface RestPageableAttributes extends CriteriaAttributes {
+
+  /**
+   * 默认页数
+   */
+  int DEFAULT_PAGE = 0;
+
+  /**
+   * 默认单页数量
+   */
+  int DEFAULT_SIZE = 20;
+
+  /**
+   * 当前页
+   */
+  String PAGE = "page";
+
+  /**
+   * 单页数量
+   */
+  String SIZE = "size";
+
+  default <T extends RestPageableAttributes> T setPage(int page) {
+    return set(PAGE, page);
+  }
+
+  default int getPage() {
+    return getExpect(PAGE, DEFAULT_PAGE);
+  }
+
+  default <T extends RestPageableAttributes> T setSize(int size) {
+    return set(SIZE, size);
+  }
+
+  default int getSize() {
+    return getExpect(SIZE, DEFAULT_SIZE);
+  }
+}
