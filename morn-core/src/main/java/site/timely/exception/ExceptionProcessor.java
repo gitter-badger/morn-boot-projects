d1126e29aa10b695473a99b1aa65f4c2a4e4c282
timely-rain <lolic_sd@hotmail.com>
2018-10-17 21:34:32 +0800

    新增异常处理器

---
+++ morn-core/src/main/java/site/timely/exception/ExceptionProcessor.java
@@ -0,0 +1,18 @@
+package site.timely.exception;
+
+/**
+ * 异常处理器
+ *
+ * @author timely-rain
+ * @version 1.0.0, 2018/10/16
+ * @since 1.0
+ */
+public interface ExceptionProcessor {
+
+  /**
+   * 处理异常
+   *
+   * @return 异常消息
+   */
+  <T extends Exception> ExceptionMessage process(T exception);
+}
