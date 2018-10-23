9ec14c042b968546a003d8f476cb20d501954488
timely-rain <lolic_sd@hotmail.com>
2018-10-23 22:07:22 +0800

    新增Http工具类

---
+++ morn-core/src/main/java/site/morn/util/HttpUtils.java
@@ -0,0 +1,30 @@
+package site.morn.util;
+
+/**
+ * Http工具类
+ *
+ * @author timely-rain
+ * @version 1.0.0, 2018/10/23
+ * @since 1.0
+ */
+public final class HttpUtils {
+
+  private HttpUtils() {
+  }
+
+  public final class Header {
+
+    private Header() {
+    }
+
+    public static final String CONTENT_TYPE = "Content-Type";
+  }
+
+  public final class ContentType {
+
+    private ContentType() {
+    }
+
+    public static final String JSON = "application/json";
+  }
+}
