3c945e6b92213867835ab86351df86958fed64ce
timely-rain <lolic_sd@hotmail.com>
2018-10-30 22:12:43 +0800

    Initial commit

---
+++ morn-core/src/main/java/site/morn/constant/MetricUnit.java
@@ -0,0 +1,22 @@
+package site.morn.constant;
+
+/**
+ * 度量单位
+ *
+ * @author timely-rain
+ * @since 1.0, 2018/10/25
+ */
+public final class MetricUnit {
+
+  private MetricUnit() {
+  }
+
+  public static final class Text {
+
+    private Text() {
+    }
+
+    public static final Integer SHORT = 32;
+  }
+
+}
