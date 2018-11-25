179faed80b1ac16af14e2a9a47e93dbf2939fc61
timely-rain <lolic_sd@hotmail.com>
2018-11-25 22:49:59 +0800

    完善BeanIdentify的Javadoc

--- morn-core/src/main/java/site/morn/bean/BeanIdentify.java
+++ morn-core/src/main/java/site/morn/bean/BeanIdentify.java
@@ -5,15 +5,20 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * 实例注解标识
+ * 实例标识
+ *
+ * <p>实例标识用于描述实例的唯一标识、标签、用途
  *
  * @author timely-rain
+ * @see site.morn.bean.annotation.Name
+ * @see site.morn.bean.annotation.Tag
+ * @see site.morn.bean.annotation.Target
  * @since 1.0.0, 2018/11/25
  */
 @Getter
 @Setter
 @Builder
-public class BeanAnnotationIdentify implements AnnotationIdentify {
+public class BeanIdentify implements AnnotationIdentify {
 
   /**
    * 名称
