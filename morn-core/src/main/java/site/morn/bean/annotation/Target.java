7cedc399230373c7c6475846c09b413a3b1b0431
timely-rain <lolic_sd@hotmail.com>
2018-11-25 22:20:54 +0800

    声明Target的滞留周期

--- morn-core/src/main/java/site/morn/bean/annotation/Target.java
+++ morn-core/src/main/java/site/morn/bean/annotation/Target.java
@@ -1,13 +1,17 @@
 package site.morn.bean.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * 目标注解
  *
- * <p>目标是唯一的，通常代表实例可处理的对象类型
+ * <p>目标是唯一的，通常代表实例的处理类型
  *
  * @author timely-rain
  * @since 1.0.0, 2018/11/25
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Target {
 
   /**
@@ -15,5 +19,5 @@ public @interface Target {
    *
    * @return 目标
    */
-  Class<?> getTarget();
+  Class<?> value();
 }
