77687f69afad530f3ce5e2b38a8fc3b3dfbd9624
timely-rain <lolic_sd@hotmail.com>
2018-11-29 16:33:50 +0800

    Rest新增from、to的转换外来消息，对外隐藏了RestConverter

--- morn-core/src/main/java/site/morn/rest/Rests.java
+++ morn-core/src/main/java/site/morn/rest/Rests.java
@@ -1,28 +1,47 @@
 package site.morn.rest;
 
-import java.util.Objects;
-import site.morn.rest.convert.RestConvertHolder;
+import java.util.List;
+import site.morn.bean.IdentifiedBeanCache;
 import site.morn.rest.convert.RestConverter;
 import site.morn.translate.Transfer;
-import site.morn.translate.Transfers;
-import site.morn.translate.TranslateHolder;
 import site.morn.translate.Translator;
 
 /**
  * REST工具类
  *
  * @author timely-rain
- * @version 1.0.0, 2018/7/25
- * @since 1.0
+ * @since 1.0.0, 2018/7/25
  */
 public class Rests {
 
-  public static final String CODE_OK = "morn.ok";
-  public static final String CODE_ERROR = "morn.error";
+  /**
+   * 成功状态码
+   */
+  private static final String CODE_OK = "morn.ok";
 
+  /**
+   * 错误状态码
+   */
+  private static final String CODE_ERROR = "morn.error";
+
+  /**
+   * 标识实例缓存
+   */
+  private static IdentifiedBeanCache beanCache;
+
+  /**
+   * 翻译器
+   */
+  private static Translator translator;
+
+  /**
+   * 消息体
+   */
   private RestMessage entity;
-  private RestConverter converter;
-  private Translator translator;
+
+  /**
+   * 翻译载体
+   */
   private Transfer transfer;
 
   private Rests() {
@@ -37,7 +56,7 @@ public class Rests {
   }
 
   public static Rests buildError() {
-    return build().translate(CODE_ERROR).success(false).level(RestMessage.Level.Error);
+    return buildError(CODE_ERROR).success(false).level(RestMessage.Level.Error);
   }
 
   public static Rests buildError(String code, Object... args) {
@@ -65,6 +84,43 @@ public class Rests {
   }
 
   /**
+   * 根据外来消息生成REST消息
+   *
+   * @param foreign 外来消息
+   * @param <T> 外来消息类型
+   * @return REST消息
+   * @see RestConverter REST消息转换器
+   */
+  public static <T> RestMessage from(T foreign) {
+    List<RestConverter> restConverters = beanCache
+        .searchByTarget(RestConverter.class, foreign.getClass());
+    if (restConverters.size() == 0) {
+      return null;
+    }
+    RestConverter<T> restConverter = restConverters.get(0);
+    return restConverter.prototype(foreign);
+  }
+
+  /**
+   * 根据REST消息生成外来消息
+   *
+   * @param restMessage REST消息
+   * @param foreign 外来消息
+   * @param <T> 外来消息类型
+   * @return 外来消息
+   * @see RestConverter REST消息转换器
+   */
+  public static <T> T to(RestMessage restMessage, Class<T> foreign) {
+    List<RestConverter> restConverters = beanCache
+        .searchByTarget(RestConverter.class, foreign);
+    if (restConverters.size() == 0) {
+      return null;
+    }
+    RestConverter<T> restConverter = restConverters.get(0);
+    return restConverter.generic(restMessage);
+  }
+
+  /**
    * 设置REST消息体
    *
    * @return REST消息体
@@ -74,49 +130,9 @@ public class Rests {
     return this;
   }
 
-  /**
-   * 设置翻译器
-   *
-   * @return REST工具类
-   */
-  public Rests translator(String name) {
-    this.translator = TranslateHolder.translator(name);
-    return this;
-  }
-
-  /**
-   * 设置翻译器
-   *
-   * @return REST工具类
-   */
-  public Rests translator(Translator translator) {
-    this.translator = translator;
-    return this;
-  }
-
-  /**
-   * 设置转换器
-   *
-   * @return REST工具类
-   */
-  public Rests converter(String name) {
-    this.converter = RestConvertHolder.getConverter(name);
-    return this;
-  }
-
-  /**
-   * 设置转换器
-   *
-   * @return REST工具类
-   */
-  public Rests converter(RestConverter converter) {
-    this.converter = converter;
-    return this;
-  }
-
   public Rests translate(String code, Object... args) {
-    this.transfer = Transfers.transfer(code, args);
-    return this.code(code).message(transfer.getMessage());
+    this.transfer = Transfer.builder().code(code).args(args).build();
+    return this.code(code);
   }
 
   public Rests success(boolean value) {
@@ -145,24 +161,24 @@ public class Rests {
   }
 
   public RestMessage generate() {
-    if (Objects.isNull(translator)) {
-      translator = TranslateHolder.defaultTranslator();
-    }
-    String message = translator.translate(transfer.getMessage(), transfer.getArgs());
+    String message = translator.translate(transfer.getMessageCode(), transfer.getArgs());
     return entity.message(message);
   }
 
-  @SuppressWarnings("unchecked")
-  public <T> T convert() {
-    RestMessage restMessage = generate();
-    RestConverter<T> restConverter = this.converter;
-    return restConverter.generic(restMessage);
-  }
-
-  @SuppressWarnings("unchecked")
-  public <T> T convert(Class<T> cls) {
-    RestMessage restMessage = generate();
-    RestConverter<T> restConverter = this.converter;
-    return restConverter.generic(restMessage);
+  /**
+   * 根据当前REST消息，生成外来消息
+   *
+   * @param foreign 外来消息类
+   * @param <T> 外来消息类型
+   * @return 外来消息
+   */
+  public <T> T to(Class<T> foreign) {
+    List<RestConverter> restConverters = beanCache
+        .searchByTarget(RestConverter.class, foreign);
+    if (restConverters.size() == 0) {
+      return null;
+    }
+    RestConverter<T> restConverter = restConverters.get(0);
+    return restConverter.generic(this.entity);
   }
 }
