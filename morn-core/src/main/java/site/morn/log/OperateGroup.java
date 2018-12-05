a8731d10342bafefbf98384bec133ac74fe79a1a
timely-rain <lolic_sd@hotmail.com>
2018-12-05 22:10:56 +0800

    修改OperateGroup的作用域

--- morn-core/src/main/java/site/morn/log/OperateGroup.java
+++ morn-core/src/main/java/site/morn/log/OperateGroup.java
@@ -1,9 +1,11 @@
 package site.morn.log;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * 操作组
@@ -14,6 +16,7 @@ import java.lang.annotation.RetentionPolicy;
 @Documented
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface OperateGroup {
 
   /**
