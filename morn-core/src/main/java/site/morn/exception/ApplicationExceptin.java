dfd1ad9654d6f08e90910bddb311edfdbfe4c8e6
timely-rain <lolic_sd@hotmail.com>
2018-12-10 00:10:37 +0800

    新增应用异常类

---
+++ morn-core/src/main/java/site/morn/exception/ApplicationExceptin.java
@@ -0,0 +1,22 @@
+package site.morn.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 应用异常
+ *
+ * @author timely-rain
+ * @since 1.0.0, 2018/12/9
+ */
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class ApplicationExceptin extends RuntimeException {
+
+  /**
+   * 异常消息
+   */
+  private final ExceptionMessage exceptionMessage;
+}
