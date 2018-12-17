433c02ffe2ae58f497f387bea4e8f84ead429de0
timely-rain <lolic_sd@hotmail.com>
2018-12-17 23:13:36 +0800

    新增并引用REST构建器工具类

---
+++ morn-core/src/main/java/site/morn/rest/RestBuilders.java
@@ -0,0 +1,54 @@
+package site.morn.rest;
+
+import site.morn.rest.RestMessage.Level;
+
+/**
+ * REST构建器工具类
+ *
+ * @author timely-rain
+ * @since 1.0.0, 2018/12/17
+ */
+public class RestBuilders {
+
+  /**
+   * 成功状态码
+   */
+  private static final String CODE_OK = "rest.ok";
+
+  /**
+   * 错误状态码
+   */
+  private static final String CODE_ERROR = "rest.fail";
+
+  private RestBuilders() {
+  }
+
+  private static Rests builder() {
+    return Rests.builder();
+  }
+
+  public static Rests infoBuilder() {
+    return builder().success(true).level(Level.INFO);
+  }
+
+  public static Rests errorBuilder() {
+    return builder().success(false).level(Level.ERROR);
+  }
+
+  public static Rests successBuilder() {
+    return infoBuilder().code(CODE_OK).translate(CODE_OK);
+  }
+
+  public static Rests successBuilder(String code, Object... args) {
+    return infoBuilder().code(code).translate(code, args);
+  }
+
+  public static Rests failureBuilder() {
+    return failureBuilder(CODE_ERROR);
+  }
+
+  public static Rests failureBuilder(String code, Object... args) {
+    return errorBuilder().code(code).translate(code, args);
+  }
+
+}
--- morn-core/src/main/java/site/morn/rest/Rests.java
+++ morn-core/src/main/java/site/morn/rest/Rests.java
@@ -8,7 +8,7 @@ import site.morn.translate.Transfer;
 import site.morn.translate.Translator;
 
 /**
- * REST工具类
+ * REST构建器
  *
  * @author timely-rain
  * @since 1.0.0, 2018/7/25
@@ -64,48 +64,28 @@ public class Rests {
     Rests.translator = translator;
   }
 
-  private static Rests pure() {
+  public static Rests builder() {
     return new Rests().restMessage(new SimpleRestMessage());
   }
 
-  private static Rests pureOk() {
-    return pure().success(true).level(Level.INFO);
-  }
-
-  private static Rests pureError() {
-    return pure().success(false).level(Level.ERROR);
-  }
-
-  public static Rests buildOk() {
-    return pureOk().code(CODE_OK).translate(CODE_OK);
-  }
-
-  public static Rests buildError() {
-    return pureError().code(CODE_ERROR).translate(CODE_ERROR);
-  }
-
-  public static Rests buildError(String code, Object... args) {
-    return pureError().code(code).translate(code, args);
-  }
-
   public static RestMessage ok() {
-    return buildOk().build();
+    return RestBuilders.successBuilder().build();
   }
 
-  public static RestMessage ok(String message) {
-    return buildOk().message(message).build();
+  public static RestMessage ok(String code, Object... args) {
+    return RestBuilders.successBuilder(code, args).build();
   }
 
   public static RestMessage ok(Object data) {
-    return buildOk().data(data).build();
+    return RestBuilders.successBuilder().data(data).build();
   }
 
   public static RestMessage error() {
-    return buildError().build();
+    return RestBuilders.failureBuilder().build();
   }
 
   public static RestMessage error(String code, Object... args) {
-    return buildError(code, args).build();
+    return RestBuilders.failureBuilder(code, args).build();
   }
 
   /**
@@ -142,61 +122,6 @@ public class Rests {
   }
 
   /**
-   * 设置REST消息体
-   *
-   * @return REST消息体
-   */
-  public Rests restMessage(RestMessage entity) {
-    this.restMessage = entity;
-    return this;
-  }
-
-  public Rests success(boolean value) {
-    restMessage.setSuccess(value);
-    return this;
-  }
-
-  public Rests translate(String code, Object... args) {
-    this.transfer = Transfer.builder().code(code).args(args).build();
-    return this;
-  }
-
-  public Rests code(String value) {
-    restMessage.setCode(value);
-    return this;
-  }
-
-  public Rests level(Level level) {
-    restMessage.setLevel(level);
-    return this;
-  }
-
-  public Rests message(String value) {
-    restMessage.setMessage(value);
-    return this;
-  }
-
-  public Rests data(Object value) {
-    restMessage.setData(value);
-    return this;
-  }
-
-  /**
-   * 构建REST消息
-   *
-   * <p>构建消息时，会进行翻译{@link Translator#translate(Transfer)}操作。
-   * 如果显示的指定了{@link Rests#message(String)}，则不会进行翻译。
-   *
-   * @return REST消息
-   */
-  public RestMessage build() {
-    if (Objects.isNull(restMessage.getMessage())) {
-      restMessage.setMessage(translator.translate(transfer));
-    }
-    return restMessage;
-  }
-
-  /**
    * 根据当前REST消息，生成外来消息
    *
    * @param foreign 外来消息类
@@ -209,5 +134,97 @@ public class Rests {
       return null;
     }
     return restConverter.convert(build());
+  }
+
+  /**
+   * 构建REST消息
+   *
+   * <p>构建消息时，会进行翻译操作{@link Rests#translate(String, Object...)}。
+   * 如果显示的指定了消息内容{@link Rests#message(String)}，则不会进行翻译。
+   *
+   * @return REST消息
+   */
+  public RestMessage build() {
+    if (Objects.isNull(restMessage.getMessage())) {
+      restMessage.setMessage(translator.translate(transfer));
+    }
+    return restMessage;
+  }
+
+  /**
+   * 设置REST消息体
+   *
+   * @return REST消息体
+   */
+  public Rests restMessage(RestMessage restMessage) {
+    this.restMessage = restMessage;
+    return this;
+  }
+
+  /**
+   * 设置国际化载体
+   *
+   * @param code 国际化编码
+   * @param args 国际化参数
+   * @return REST构建器
+   */
+  public Rests translate(String code, Object... args) {
+    this.transfer = Transfer.builder().code(code).args(args).build();
+    return this;
+  }
+
+  /**
+   * 设置成功标识
+   *
+   * @param value 成功标识
+   * @return REST构建器
+   */
+  public Rests success(boolean value) {
+    restMessage.setSuccess(value);
+    return this;
+  }
+
+  /**
+   * 设置消息级别
+   *
+   * @param level 消息级别
+   * @return REST构建器
+   */
+  public Rests level(Level level) {
+    restMessage.setLevel(level);
+    return this;
+  }
+
+  /**
+   * 设置状态码
+   *
+   * @param value 状态码
+   * @return REST构建器
+   */
+  public Rests code(String value) {
+    restMessage.setCode(value);
+    return this;
+  }
+
+  /**
+   * 设置消息内容
+   *
+   * @param value 消息内容
+   * @return REST构建器
+   */
+  public Rests message(String value) {
+    restMessage.setMessage(value);
+    return this;
+  }
+
+  /**
+   * 设置消息数据
+   *
+   * @param value 消息数据
+   * @return REST构建器
+   */
+  public Rests data(Object value) {
+    restMessage.setData(value);
+    return this;
   }
 }
